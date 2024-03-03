fe46d73b049339a587fa0d1eff3c6143e1148a30
sophiewhittle <sophiewhittle89@gmail.com>
2024-03-03 10:01:40 -0500

    Refactor, remove commented code, update model

--- Program.cs
+++ Program.cs
@@ -8,21 +8,25 @@ using NpgsqlTypes;
 
 internal class Program
 {
-    static async Task Main(string[] args)
+    private const int NUMBER_OF_DAYS = 3;
+
+    static void Main(string[] args)
     {
-        for (int i = 1; i < 4; i++)
+        var allRecords = new List<TWCapacity>();
+
+        for (int i = 1; i <= NUMBER_OF_DAYS; i++)
         {
             var date = DateTime.Today.AddDays(-i);
 
             var url = GenerateUrl(date);
 
-            var res = GetCSV(url);
-
-            InsertAll(res, date);
+            allRecords.AddRange(GetCSV(url, date));
         }
+
+        InsertMany(allRecords);
     }
 
-    static async void InsertAll(IEnumerable<TWCapacity> items, DateTime date) // does this need to be async?
+    static void InsertMany(IEnumerable<TWCapacity> items) 
     {
         string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString_Postgres"].ConnectionString;
         NpgsqlConnection connection = new NpgsqlConnection(connectionString);
@@ -37,27 +41,27 @@ internal class Program
 
             foreach (var item in items)
             {
-                await importer.StartRowAsync();
-                await importer.WriteAsync(item.Loc);
-                await importer.WriteAsync(item.LocZn);
-                await importer.WriteAsync(item.LocName);
-                await importer.WriteAsync(item.LocPurpDesc);
-                await importer.WriteAsync(item.LocQTI);
-                await importer.WriteAsync(item.FlowInd);
-                await importer.WriteAsync(item.DC);
-                await importer.WriteAsync(item.OPC);
-                await importer.WriteAsync(item.TSQ);
-                await importer.WriteAsync(item.OAC);
-                await importer.WriteAsync(item.IT == 'Y' ? true : false);
-                await importer.WriteAsync(item.AuthOverrunInd == 'Y' ? true : false);
-                await importer.WriteAsync(item.NomCapExceedInd == 'Y' ? true : false);
-                await importer.WriteAsync(item.AllQtyAvail == 'Y' ? true : false);
-                await importer.WriteAsync(item.QtyReason);
-                await importer.WriteAsync(date.Date, NpgsqlDbType.Date);
-                await importer.WriteAsync(DateTime.Now);
+                importer.StartRow();
+                importer.Write(item.Loc);
+                importer.Write(item.LocZn);
+                importer.Write(item.LocName);
+                importer.Write(item.LocPurpDesc);
+                importer.Write(item.LocQTI);
+                importer.Write(item.FlowInd);
+                importer.Write(item.DC);
+                importer.Write(item.OPC);
+                importer.Write(item.TSQ);
+                importer.Write(item.OAC);
+                importer.Write(item.IT == 'Y' ? true : false);
+                importer.Write(item.AuthOverrunInd == 'Y' ? true : false);
+                importer.Write(item.NomCapExceedInd == 'Y' ? true : false);
+                importer.Write(item.AllQtyAvail == 'Y' ? true : false);
+                importer.Write(item.QtyReason);
+                importer.Write(item.Date, NpgsqlDbType.Date);
+                importer.Write(DateTime.Now);
             }
 
-            await importer.CompleteAsync();
+            importer.Complete();
         }
         catch (Exception ex)
         {
@@ -72,49 +76,30 @@ internal class Program
     static string GenerateUrl(DateTime date)
     {
         // Final cycle
-        var url = "https://twtransfer.energytransfer.com/ipost/capacity/operationally-available?f=csv&extension=csv&asset=TW&cycle=5&searchType=NOM&searchString=&locType=ALL&locZone=ALL&gasDay=" +
-            date.Month +
-            "%2F" +
-            date.Day +
-            "%2F" +
-            date.Year +
-            "";
+        var url = $"https://twtransfer.energytransfer.com/ipost/capacity/operationally-available?" +
+                  $"f=csv&extension=csv&asset=TW&gasDay={date.Month}%2F{date.Day}%2F{date.Year}&cycle=3&searchType=NOM&searchString=&locType=ALL&locZone=ALL";
 
         return url;
     }
 
-    static List<TWCapacity> GetCSV(string url)
+    static List<TWCapacity> GetCSV(string url, DateTime date)
     {
         var records = new List<TWCapacity>();
 
         HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
         HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
-        using (var reader = new StreamReader(response.GetResponseStream())) // what is this doing?
+        using (var reader = new StreamReader(response.GetResponseStream())) 
         using (CsvReader csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
         {
             csvReader.Context.RegisterClassMap<TWCapacityMap>();
 
-            //while (csvReader.Read())
-            //{
-            //    // TODO: currently the code will skip any records that it cannot parse - maybe it should skip the whole batch?
-            //    try
-            //    {
-            //        var record = csvReader.GetRecord<TWCapacity>();
-
-            //        result.Add(record);
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        Console.WriteLine($"Error: {ex.Message}");
-            //    }
-            //}
-
             try
             {
-                records = (List<TWCapacity>)csvReader.GetRecords<TWCapacity>().ToList();
+                records = csvReader.GetRecords<TWCapacity>().ToList();
 
-                //result.AddRange(records);
+                foreach (var record in records)
+                    record.Date = date.Date;
             }
             catch (Exception ex)
             {
@@ -130,52 +115,4 @@ internal class Program
     {
         return true;
     }
-
-    //static async Task Insert(TWCapacity twCapacity)
-    //{
-    //    string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString_Postgres"].ConnectionString;
-    //    NpgsqlConnection connection = new NpgsqlConnection(connectionString);
-
-    //    try
-    //    {
-    //        connection.Open();
-
-    //        var TABLE_NAME = "tw_capacity";
-
-    //        string commandText = $"INSERT INTO {TABLE_NAME} " +
-    //                             $"(loc, loc_zn, loc_name, loc_purp_desc, loc_qti, flow_ind, dc, opc, tsq, oac, it, auth_overrun_ind, nom_cap_exceed_ind, all_qty_avail, qty_reason, date_inserted) " +
-    //                             $"VALUES (@loc, @locZn, @locName, @locPurpDesc, @locQti, @flowInd, @dc, @opc, @tsq, @oac, @it, @authOverrunInd, @nomCapExceedInd, @allQtyAvail, @qtyReason, @dateInserted)";
-
-    //        await using (var cmd = new NpgsqlCommand(commandText, connection))
-    //        {
-    //            cmd.Parameters.AddWithValue("loc", twCapacity.Loc);
-    //            cmd.Parameters.AddWithValue("locZn", twCapacity.LocZn);
-    //            cmd.Parameters.AddWithValue("locName", twCapacity.LocName);
-    //            cmd.Parameters.AddWithValue("locPurpDesc", twCapacity.LocPurpDesc);
-    //            cmd.Parameters.AddWithValue("locQti", twCapacity.LocQTI);
-    //            cmd.Parameters.AddWithValue("flowInd", twCapacity.FlowInd);
-    //            cmd.Parameters.AddWithValue("dc", twCapacity.DC);
-    //            cmd.Parameters.AddWithValue("opc", twCapacity.OPC);
-    //            cmd.Parameters.AddWithValue("tsq", twCapacity.TSQ);
-    //            cmd.Parameters.AddWithValue("oac", twCapacity.OAC);
-    //            cmd.Parameters.AddWithValue("it", twCapacity.IT == 'Y' ? true : false);
-    //            cmd.Parameters.AddWithValue("authOverrunInd", twCapacity.AuthOverrunInd == 'Y' ? true : false);
-    //            cmd.Parameters.AddWithValue("nomCapExceedInd", twCapacity.NomCapExceedInd == 'Y' ? true : false);
-    //            cmd.Parameters.AddWithValue("allQtyAvail", twCapacity.AllQtyAvail == 'Y' ? true : false);
-    //            cmd.Parameters.AddWithValue("qtyReason", twCapacity.QtyReason);
-    //            cmd.Parameters.AddWithValue("dateInserted", DateTime.Now);
-
-    //            await cmd.ExecuteNonQueryAsync();
-    //        }
-
-    //    }
-    //    catch (Exception ex)
-    //    {
-    //        Console.WriteLine($"Error: {ex.Message}");
-    //    }
-    //    finally
-    //    {
-    //        connection.Close();
-    //    }
-    //}
 }
--- TWCapacity.cs
+++ TWCapacity.cs
@@ -2,21 +2,22 @@
 {
     public class TWCapacity 
     {
-        public int Loc { get; set; }
+        public int? Loc { get; set; }
         public string LocZn { get; set; }
         public string LocName { get; set; }
         public string LocPurpDesc { get; set; }
         public string LocQTI { get; set; }
         public string FlowInd { get; set; }
         public int? DC { get; set; }
-        public int OPC { get; set; }
-        public int TSQ { get; set; }
-        public int OAC { get; set; }
-        public char IT { get; set; }
-        public char AuthOverrunInd { get; set; }
-        public char NomCapExceedInd { get; set; }
-        public char AllQtyAvail { get; set; }
+        public int? OPC { get; set; }
+        public int? TSQ { get; set; }
+        public int? OAC { get; set; }
+        public char? IT { get; set; }
+        public char? AuthOverrunInd { get; set; }
+        public char? NomCapExceedInd { get; set; }
+        public char? AllQtyAvail { get; set; }
         public string QtyReason { get; set; }
+        public DateTime? Date { get; set; }
     }
 }
 
